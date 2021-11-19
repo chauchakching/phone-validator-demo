60ec5b5f37b941083711f9f4cf50d3f928aa9f2b
chauchakching <chauchakching@gmail.com>
2021-11-20 01:00:56 +0800

    feat: create first input, button and form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { Button, Input } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
 function App () {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log(data)
+    console.log('phone:', data.get('phone'))
+  }
   return (
     <div className="App">
-
+      <Box component="form" onSubmit={handleSubmit}>
+        phone number:
+        <Input name="phone" id="phone" />
+        <Button type="submit">validate</Button>
+      </Box>
     </div>
   )
 }
