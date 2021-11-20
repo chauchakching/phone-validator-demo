6572486b795bea809448b48de1594f18bfe05799
chauchakching <chauchakching@gmail.com>
2021-11-20 12:25:07 +0800

    feat: save previous phone validation results

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,14 +9,21 @@ import {
   TableRow
 } from '@mui/material'
 import { Box } from '@mui/system'
+import { reverse } from 'fp-ts/lib/Array'
 import React, { useState } from 'react'
 import {
   PhoneValidationResult,
   validatePhone
 } from './services/phoneValidatorService'
+import { css } from '@emotion/css'
+
+type Result = {
+  phone: string;
+  validationResult: PhoneValidationResult;
+};
 
 function App () {
-  const [result, setResult] = useState<PhoneValidationResult>()
+  const [results, setResults] = useState<Result[]>([])
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const data = new FormData(event.currentTarget)
@@ -25,7 +32,7 @@ function App () {
     console.log('phone:', phone)
     if (!phone) return
     const validationResult = await validatePhone(phone)
-    setResult(validationResult)
+    setResults([...results, { phone, validationResult: validationResult }])
   }
   return (
     <div className="App">
@@ -33,19 +40,34 @@ function App () {
         phone number:
         <Input name="phone" id="phone" />
         <Button type="submit">validate</Button>
-        {result && (
-          <TableContainer component={Paper}>
-            <Table size="small">
-              <TableBody>
-                {Object.entries(result).map(([key, val]) => (
-                  <TableRow key={key}>
-                    <TableCell>{key}</TableCell>
-                    <TableCell>{val}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        {reverse(
+          results.map((result, index) => (
+            <div
+              key={index}
+              className={css`
+                margin-bottom: 12px;
+              `}
+            >
+              <TableContainer component={Paper}>
+                <Table size="small">
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>phone</TableCell>
+                      <TableCell>{result.phone}</TableCell>
+                    </TableRow>
+                    {Object.entries(result.validationResult).map(
+                      ([key, val]) => (
+                        <TableRow key={key}>
+                          <TableCell>{key}</TableCell>
+                          <TableCell>{String(val)}</TableCell>
+                        </TableRow>
+                      )
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          ))
         )}
       </Box>
     </div>
